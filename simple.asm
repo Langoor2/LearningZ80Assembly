4876d502a7628958d21a4368447d2f2f332825f3
Langoor2 <nielsdevries@riseup.net>
2016-11-11 23:03:34 +0100

    change stuff

--- simple.asm
+++ simple.asm
@@ -1,17 +1,19 @@
 BDOS: EQU 5
 org 100h
+LD HL,TextInput
 LD HL,text1
 Call printstring
 LD HL,text2
 Call printstring
-
+LD HL,TextInput
 LD B,7
 ;7 character text input
 .loop:
   PUSH BC
   LD C,8
   CALL BDOS
-  LD [TextInput],A
+  LD [HL],A
+  INC HL
   POP BC
 djnz .loop
 
