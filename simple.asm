f75b9769d4330350db981654a3bcbc0415738546
Langoor2 <nielsdevries@riseup.net>
2016-11-11 18:58:23 +0100

    change all of it

--- simple.asm
+++ simple.asm
@@ -1,11 +1,22 @@
 org 100h
-ld c,9
-ld de,hello
-ld A,[HL]
+ld DE,text1
+ld DE,text2
 call 5
-hello: db "yo sup",13,10,"$"
+text1: db "Yo sup!",13,10,"$"
+text2: db "sup?",13,10,"$"
 ld c,9
 ld de,hello2
 call 5
 RET
-hello2: db "hoi",13,10,"$"
+
+
+printstring: ld A,[HL]        ;Input: HL = pointer to string
+CP "$"
+RET Z
+LD E,A
+LD C,2                        ;Conout
+PUSH HL                       ;Protect, because we care about them <3
+CALL BDOS
+POP HL                        ;Retrive them from stack
+INCL HL                       ;Lets get the next one
+JP printstring                ;Lets roll again!
