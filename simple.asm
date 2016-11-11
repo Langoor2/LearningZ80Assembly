a72d275a542e6fbcba4c4f385113914742b917b4
Langoor2 <nielsdevries@riseup.net>
2016-11-11 19:19:43 +0100

    add nice way to print strings

--- simple.asm
+++ simple.asm
@@ -1,15 +1,16 @@
 org 100h
-ld c,9
-ld DE,text2
-call 5
-ld c,9
-ld de,text1
+LD HL,text1
+Call printstring
+LD HL,text2
+Call printstring
 call 5
 RET
 
 text1: db "Yo sup!",13,10,"$"
 text2: db "sup?",13,10,"$"
-printstring: ld A,[HL]        ;Input: HL = pointer to string
+;Input: HL = pointer to string
+printstring: ld A,[HL]
+BDOS: EQU 5
 CP "$"
 RET Z
 LD E,A
