35b0f657b1a6a65590165d83516b2ca096457635
Langoor2 <nielsdevries@riseup.net>
2016-11-11 21:24:47 +0100

    add text input

--- simple.asm
+++ simple.asm
@@ -4,11 +4,26 @@ LD HL,text1
 Call printstring
 LD HL,text2
 Call printstring
+LD C,8
+CALL BDOS
 RET
+
+
+LD C,8
+CALL BDOS
+LD E,A
+LD C,2
+CALL BDOS
+
+TextInput: db 0
+LD C,8
+CALL BDOS
+LD [TextInput],A
 text1: db "Yo sup!",13,10,"$"
 text2: db "sup?",13,10,"$"
 ;Input: HL = pointer to string
 printstring: ld A,[HL]
+
 CP "$"
 RET Z
 LD E,A
