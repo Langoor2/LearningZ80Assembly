c33661b1d6e4e720105538c975386cb5493a6b55
Langoor2 <nielsdevries@riseup.net>
2016-11-11 19:30:57 +0100

    change text markup

--- simple.asm
+++ simple.asm
@@ -1,16 +1,14 @@
+BDOS: EQU 5
 org 100h
 LD HL,text1
 Call printstring
 LD HL,text2
 Call printstring
-call 5
 RET
-
 text1: db "Yo sup!",13,10,"$"
 text2: db "sup?",13,10,"$"
 ;Input: HL = pointer to string
 printstring: ld A,[HL]
-BDOS: EQU 5
 CP "$"
 RET Z
 LD E,A
